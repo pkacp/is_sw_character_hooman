214e7941ae3f1f67446978f82320cbb6817e1774
pkacp <p.kacprzak.96@gmail.com>
2021-02-25 21:17:33 +0100

    Some api shared specs

--- spec/people_spec.rb
+++ spec/people_spec.rb
@@ -3,4 +3,9 @@ require_relative '../lib/sw_api/people.rb'
 RSpec.describe SWApi::People do
   it_behaves_like "a star wars api class"
 
+  before :all do
+    @people_type = 'people'
+  end
+
+
 end
--- spec/support/sw_api_spec.rb
+++ spec/support/sw_api_spec.rb
@@ -1,6 +1,8 @@
 shared_examples 'a star wars api class' do
   before :all do
     @base_url = 'https://swapi.dev/api/'
+    @fake_type = 'fake_type'
+    @sample_described_class_url = @base_url + @fake_type
   end
 
   describe '.base_url' do
@@ -9,15 +11,29 @@ shared_examples 'a star wars api class' do
     end
   end
 
+  describe '.type' do
+    it 'should return string' do
+      expect(described_class.type).to be_an_instance_of String
+    end
+  end
+
   describe '.url' do
     it 'should start with sw api url' do
       expect(described_class.url).to start_with(@base_url)
     end
+
+    it 'should end with defined type' do
+      expect(described_class.url).to end_with(@fake_type)
+    end
   end
 
   describe '.search' do
-    subject { described_class.search('sample') }
-    it_behaves_like "a web request"
+    before :all do
+      @sample_searched_text = 'sample'
+      @expected_request_path = "#{@sample_described_class_url}/?search=#{@sample_searched_text}"
+    end
+    subject { described_class.search(@sample_searched_text) }
+    it_behaves_like "a web request", @expected_request_path
 
     it 'should take one argument' do
       expect(described_class).to respond_to(:search).with(1).argument
@@ -26,17 +42,16 @@ shared_examples 'a star wars api class' do
     # First approach .to_s
     # Second approach String()
     # Third approach is an instance of String
-    it 'argument should behave like string' do
+    xit 'argument should behave like string' do
       expect { described_class.search(:r2) }.not_to raise_error ArgumentError
       expect { described_class.search('r2') }.not_to raise_error ArgumentError
       expect { described_class.search(nil) }.not_to raise_error ArgumentError
+      expect { described_class.search(96) }.not_to raise_error ArgumentError
     end
 
     it 'should create correct query with searched text' do
       # given
-      searched_text = 'yoda'
-      fake_described_type = 'fake_described_type'
-      search_request = stub_request(:get, "#{@base_url}#{fake_described_type}/?search=#{searched_text}")
+      search_request = stub_request(:get, @expected_request_path)
 
       # when
       described_class.search(searched_text)
@@ -47,10 +62,27 @@ shared_examples 'a star wars api class' do
   end
 end
 
-shared_examples 'a web request' do
-  # offline
-  # 500
-  # 400
-  # 200
+shared_examples 'a web request' do |request_url|
+  describe 'in offline situation' do
+    it 'should rise NoInternetError' do
+      stub_request(:get, request_url).to_raise(StandardError)
+      expect(subject).to raise_error ConnectionError
+    end
+  end
+
+  describe 'when server not found' do
+    it 'should return ServerNotFound' do
+      stub_request(:get, request_url).to_timeout
+      expect(subject).to raise_error ServerNotFound
+    end
+  end
+
+  describe 'when api server error' do
+    it 'should return InternalServerError' do
+      stub_request(:get, request_url).to_return(status: [500, "Internal Server Error"])
+      expect(subject).to raise_error InternalServerError
+    end
+  end
+# 200
 end
 
