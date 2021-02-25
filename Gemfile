cc9e0cfa384a5f412c22c359921393f197e15afa
pkacp <p.kacprzak.96@gmail.com>
2021-02-25 22:26:54 +0100

    Add simplecov

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 
 group :test do
   gem 'rspec'
-  # gem 'simplecov', :require => false
+  gem 'simplecov', :require => false
   gem "webmock"
   # gem 'mutant-rspec'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'webmock/rspec'
+require 'simplecov'
+SimpleCov.start
+# SimpleCov.add_filter ['spec/support']
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -47,8 +50,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
 =begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
@@ -101,5 +104,5 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 
-  Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
+  Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 end
